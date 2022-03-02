cc5b06988edf643e27ffdd1e5fc3e2476ee1f4f3
Nico Gutik <nicogutik@gmail.com>
2022-03-02 07:31:01 -0300

    agregando cambios

--- src/views/Carrito.js
+++ src/views/Carrito.js
@@ -2,6 +2,7 @@ import ItemCart from '../componentes/ItemCart/ItemCart';
 import {useContext} from 'react'
 import CartContext from "../componentes/Context/CartContext"
 import './Carrito.css'
+import {Link} from 'react-router-dom';
 
 const Carrito = () => {
 
@@ -13,8 +14,21 @@ const {items, total} = useContext(CartContext)
         <div className="CartList">
             {items.map(item =><ItemCart card = {item}/>)}
         </div>
-        <p>${total}</p>
+        {items.length > 0 ? 
+        <p className="precioTotal">Total a pagar : ${total}</p> : 
+        <div className="nadaCarrito">
+            <div className="nadaCarritoBox">
+                <p className="nadaCarritoMsj">Aún no tienes nada en el carrito</p>
+                <Link to="/Productos">
+                    <div className="irAComprar">
+                        <button type="button" className="botonComprar">Ir a comprar</button>
+                    </div>
+                </Link>
+            </div>
+        </div>
+        }
         </>
+
     )
 }
 
